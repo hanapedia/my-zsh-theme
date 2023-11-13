6bb65d2610d597f63bfa641ccbf721ea6711b77e
Hiroki <hiroki11hanada@gmail.com>
2023-11-13 23:05:04 +0900

    add powerline separators

--- mytheme.zsh
+++ mytheme.zsh
@@ -1,12 +1,23 @@
+local branch_glyph=""
+local right_black_arrow_glyph=""
+local right_arrow_glyph=""
+local left_black_arrow_glyph=""
+local left_arrow_glyph=""
+
+# Function to get separator
+function k8s_separator() {
+  echo "%K{black}%F{blue}$right_black_arrow_glyph %k%f"
+}
+
 # Function to get the current Kubernetes context
 function k8s_context() {
   local ctx=$(kubectl config current-context 2>/dev/null)
-  [ -n "$ctx" ] && echo "%F{blue}($ctx)%f "
+  [ -n "$ctx" ] && echo "%K{blue}%F{black} $ctx %k%f$(k8s_separator)"
 }
 
 # Function to get the current directory in a shortened form
 function short_pwd() {
-  echo "%F{cyan}$(pwd | sed -e "s,^$HOME,~," -e 's,\([^/]\)[^/]*/,\1/,g')%f"
+  echo "%K{black}%F{white}$(pwd | sed -e "s,^$HOME,~," -e 's,\([^/]\)[^/]*/,\1/,g') %k%f"
 }
 
 # Function to get the current Git branch and status
@@ -14,16 +25,21 @@ function git_branch() {
   local branch=$(git branch --show-current 2>/dev/null)
   if [ -n "$branch" ]; then
     local gitstatus=$(git status --porcelain=v1 2>/dev/null)
-    local color="%F{green}"
-    # if [[ -n $(echo "$gitstatus" | grep '^??') ]]; then
-    #   color="%F{red}" # untracked changes
-    elif [[ -n $(echo "$gitstatus" | grep '^ M') ]]; then
-      color="%F{red}" # modified files
+    local color="%K{green}"
+    local separator="%K{green}%F{black}$right_black_arrow_glyph %k%f"
+    local tail="%F{green}$right_black_arrow_glyph%f"
+    if [[ -n $(echo "$gitstatus" | grep '^ M') ]]; then
+      color="%K{red}" # modified files
+      separator="%K{red}%F{black}$right_black_arrow_glyph %k%f"
+      tail="%F{red}$right_black_arrow_glyph %f"
     elif [[ -n $(echo "$gitstatus" | grep '^M') ]]; then
-      color="%F{yellow}" # staged changes
+      color="%K{yellow}" # staged changes
+      separator="%K{yellow}%F{black}$right_black_arrow_glyph %k%f"
+      tail="%F{yellow}$right_black_arrow_glyph %f"
     fi
-    echo "${color}${branch}%f "
+    echo "${separator}${color}%F{black}$branch_glyph ${branch} %k%f${tail}"
   fi
 }
 
-PROMPT='$(k8s_context)$(short_pwd) $(git_branch) \n$'
+PROMPT="$(k8s_context)$(short_pwd)$(git_branch)
+%F{grey}$%f "
